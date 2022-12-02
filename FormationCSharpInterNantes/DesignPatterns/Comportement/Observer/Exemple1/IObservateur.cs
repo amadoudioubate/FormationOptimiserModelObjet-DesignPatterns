efe6d15d9fdaf86c63bf8d27db28563e75f525cc
Amadou Dioubate <dioubateamadou11@gmail.com>
2022-12-02 11:07:14 +0100

    Debut Design Patterns Observer

---
+++ FormationCSharpInterNantes/DesignPatterns/Comportement/Observer/Exemple1/IObservateur.cs
@@ -0,0 +1,8 @@
+﻿namespace FormationCSharpInterNantes.DesignPatterns.Comportement.Observer.Exemple1
+{
+    internal interface IObservateur
+    {
+        // Méthode pour mettre à jour Observateur
+        void MettreAJour(ISujet sujet); // Je met en paramètre le sujet qui a été mis à jour
+    }
+}
---
+++ FormationCSharpInterNantes/DesignPatterns/Comportement/Observer/Exemple1/ISujet.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormationCSharpInterNantes.DesignPatterns.Comportement.Observer.Exemple1
+{
+    internal interface ISujet
+    {
+        void Attacher(IObservateur observateur);
+
+        void Detacher(IObservateur observateur); // Je ne veux plus observer mon sujet
+
+        void Notifier(ISujet sujet); // Notifie moi même (ISujet => sujet) à l'Observateur
+    }
+}
